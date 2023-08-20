c0cb966e3e8e607128725758be8cc5ba594fff6b
cp-saito-shunsuke-mummy <saito_shunsuke@colorfulpalette.co.jp>
2023-08-20 09:30:17 +0900

    不要ファイルの削除

--- FastLinq.Benchmark/Factory/PrimitiveDataFactory.cs
+++
@@ -1,9 +0,0 @@
-using System.Linq;
-
-namespace DetailResearchOfLinq.Utility
-{
-    public static class PrimitiveDataFactory
-    {
-        public static int[] CreateNumberSequenceByArray(int size) => Enumerable.Range(0, size).ToArray();
-    }
-}
